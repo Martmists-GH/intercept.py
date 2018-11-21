2f189e3d0de9309fa59dafce7c4945a8fb562a0b
Martmists <martmists@gmail.com>
2018-11-21 23:16:13 +0100

    setup.py
    Signed-off-by: Martmists <martmists@gmail.com>

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.readlines()
+
+with open("README.md") as f:
+    README = f.readlines()
+
+setup(
+    name="intercept",
+    author="martmists",
+    author_email="mail@martmists.com",
+    maintainer="martmists",
+    maintainer_email="mail@martmists.com",
+    license="MIT",
+    zip_safe=False,
+    version=VERSION,
+    description="Python library for intercept, a game by bubmet",
+    long_description=README,
+    url="https://github.com/martmists/intercept.py",
+    packages=['intercept'],
+    install_requires=REQUIREMENTS,
+    keywords=["game", "intercept", "asyncio", "trio", "curio", "anyio"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: AsyncIO",
+        "Topic :: Games/Entertainment",
+        "Topic :: Communications :: Chat",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    python_requires=">=3.7"
+)
