0a4d500ed018b2a007a9631caeaeb3cbb9908d95
martmists <martmists@gmail.com>
2018-11-26 10:35:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,17 +25,20 @@ setup(
     install_requires=REQUIREMENTS,
     keywords=["game", "intercept", "asyncio", "trio", "curio", "anyio"],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 4 - Beta",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Framework :: AsyncIO",
         "Topic :: Games/Entertainment",
         "Topic :: Communications :: Chat",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
-    python_requires=">=3.7"
+    python_requires=">=3.4"
 )
